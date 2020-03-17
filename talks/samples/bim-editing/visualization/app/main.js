3288887fa72b8b1976d39a23be0ec3a32e9ecdb7
Arno Fiva <arnofiva@gmail.com>
2020-03-17 13:07:50 +0100

    Finished Visualization

--- talks/samples/bim-editing/visualization/app/main.js
+++ talks/samples/bim-editing/visualization/app/main.js
@@ -2,6 +2,7 @@ require([
   "esri/Map",
   "esri/views/SceneView",
   "esri/layers/BuildingSceneLayer",
+  "esri/layers/support/BuildingFilter",
   "esri/widgets/LayerList",
   "./../../support/widgets.js",
   "dojo/domReady!"
@@ -9,6 +10,7 @@ require([
   Map,
   SceneView,
   BuildingSceneLayer,
+  BuildingFilter,
   LayerList,
   widgetsSupport,
 ) {
@@ -21,12 +23,17 @@ require([
   const view = new SceneView({
     map,
     container: "viewDiv",
+    qualityProfile: "high",
     // camera: {"position":{"spatialReference":{"latestWkid":3857,"wkid":102100},"x":19217739.314622313,"y":-5392869.574006113,"z":85.33910750970244},"heading":133.74224876471538,"tilt":63.801079199922754},
     camera: {"position":{"spatialReference":{"latestWkid":3857,"wkid":102100},"x":-13045079.449431838,"y":4036762.002486097,"z":446.70295623037964},"heading":315.63260562077835,"tilt":72.89304450457234},
+    alphaCompositingEnabled: true,
     environment: {
       background: {
         type: "color",
-        color: [92, 92, 92, 1]
+        color: [0, 0, 0, 0]
+      },
+      lighting: {
+        directShadowsEnabled: true,
       },
       starsEnabled: false,
       atmosphereEnabled: false
@@ -35,12 +42,12 @@ require([
   widgetsSupport.createFullscreen(view);
   window.view = view;
 
-  const buildingSceneLayer = new BuildingSceneLayer({
+  const buildingLayer = new BuildingSceneLayer({
     url: "https://tiles.arcgis.com/tiles/z2tnIkrLQ2BRzr6P/arcgis/rest/services/Admin_Building_v17/SceneServer"
   });
-  buildingSceneLayer.load();
+  const bslLoad = buildingLayer.load();
 
-  var layerList = new LayerList({
+  const layerList = new LayerList({
     view: view
   });
 
@@ -49,14 +56,14 @@ require([
   });
 
   function onClick(buttonId, onclick) {
-    var button = parent.document.getElementById(buttonId);
+    const button = parent.document.getElementById(buttonId);
     if (button && onclick) {
       button.onclick = onclick;
     }
   }
 
   function addBSL() {
-    map.add(buildingSceneLayer);
+    map.add(buildingLayer);
   }
 
   function addLayerList() {
@@ -67,25 +74,138 @@ require([
 
   function filterStructural() {
 
-    buildingSceneLayer.allSublayers.forEach(l => {
+    bslLoad.then(() => {
+      buildingLayer.allSublayers.forEach(l => {
 
-      if (l.title === "Floors" || l.startWith("Structural")) {
-        l.visible = true;
-      } else {
-        l.visible = l.type === "building-group";
-      }
+        if (l.title === "Floors" || l.title.startsWith("Structural")) {
+          l.visible = true;
+        } else {
+          l.visible = l.type === "building-group";
+        }
+      });
     });
+  }
+
+  function filterWindows() {
+
+    bslLoad.then(() => {
+      buildingLayer.allSublayers.forEach(l => {
+
+        if (l.title === "Floors" || l.title === "Windows" || l.title.startsWith("Structural")) {
+          l.visible = true;
+        } else {
+          l.visible = l.type === "building-group";
+        }
+      });
+    });
+  }
+
+  function filterWalls() {
+    bslLoad.then(() => {
+      buildingLayer.allSublayers.forEach(l => {
+
+        if (
+          l.title === "Walls" || l.title === "Rooms" ||
+          l.title.startsWith("Structural") ||
+          l.modelName === "Overview") {
+          l.visible = false;
+        } else {
+          l.visible = true;
+        }
+      });
+    });
+  }
+
+  function paintWindowsGreen() {
+    bslLoad.then(() => {
+
+      const windowLayer = buildingLayer.allSublayers.find(l => l.title === "Windows");
+
+      windowLayer.renderer = {
+        type: "simple",
+        symbol: {
+          type: "mesh-3d",
+          symbolLayers: [
+            {
+              type: "fill",
+              material: {
+                color: [0, 255, 0, 0.8],
+              },
+              edges: {
+                type: "solid",
+                color: [0, 120, 0],
+                size: 1,
+              }
+            }
+          ]
+        }
+
+      };
+
+    });
+  }
+
+  let floor = 3;
+  function setFloorFilter(mode) {
+    const filterBlocks = [{
+      filterExpression:
+        `BldgLevel = ${floor}`,
+      filterMode: {
+        type: "solid"
+      }
+    }];
+
+    if (mode === "x-ray" || mode === "wire-frame") {
+      filterBlocks.push({
+          filterExpression:
+          `BldgLevel < ${floor}`,
+          filterMode: {
+            type: mode,
+            edges: {
+              type: "solid",
+              color: [105, 142, 179, 1]
+            }
+          }
+      });
+    }
+
+    const buildingFilter = new BuildingFilter({
+      filterBlocks
+    });
+
+    // set the filter in the filters array on the layer
+    buildingLayer.filters = [buildingFilter];
+    // specify which filter is the one that should be applied
+    buildingLayer.activeFilterId = buildingFilter.id;
   }
 
   onClick("addBuildingSceneLayer", addBSL);
 
   onClick("filterBuildingSceneLayer", filterStructural);
 
+  onClick("renderBuildingSceneLayer", paintWindowsGreen);
+
+  onClick("filterFloor", setFloorFilter);
+
+  onClick("filterFloorXRay", () => setFloorFilter("x-ray"));
+  onClick("filterFloorWireFrame", () => setFloorFilter("wire-frame"));
+
   // Check what slide we are showing
-  var slide = parent && parent.Reveal && parent.Reveal.getCurrentSlide().dataset.title;
+  const slide = parent && parent.Reveal && parent.Reveal.getCurrentSlide().dataset.title;
   if (slide === "slide-bsl-sublayers") {
     addBSL();
     addLayerList();
+  } else if (slide === "slide-bsl-renderer") {
+    addBSL();
+    filterWindows();
+  } else if (slide === "slide-bsl-filter-blocks") {
+    addBSL();
+    filterWalls();
+  } else if (slide === "slide-bsl-filter-modes") {
+    floor = 4;
+    addBSL();
+    filterWalls();
+    setFloorFilter();
   }
 
 });
